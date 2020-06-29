61fe13537512848969e628b3dd08e9ea382a6b14
zhilw <zhilw@cenboomh.com>
2020-06-29 18:51:40 +0800

    解决zuul代理consumer的controller报错的问题

--- microservice-zuul/src/main/java/com/cw/stu/springcloud/microservice/zuul/SpringBootZuulApplication.java
+++ microservice-zuul/src/main/java/com/cw/stu/springcloud/microservice/zuul/SpringBootZuulApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * spring cloud的网关
@@ -13,6 +14,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
+@ComponentScan(basePackages = {"com.cw.stu.springcloud.microservice.zuul.service"})
 public class SpringBootZuulApplication {
 
     public static void main(String[] args) {
---
+++ microservice-zuul/src/main/java/com/cw/stu/springcloud/microservice/zuul/service/FeignBeanFactoryPostProcessor.java
@@ -0,0 +1,27 @@
+package com.cw.stu.springcloud.microservice.zuul.service;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+/**
+ * @author WuLiangzhi  2020/06/29 18:47
+ */
+@Component
+public class FeignBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        if (containsBeanDefinition(beanFactory, "feignContext", "eurekaAutoServiceRegistration")) {
+            BeanDefinition bd = beanFactory.getBeanDefinition("feignContext");
+            bd.setDependsOn("eurekaAutoServiceRegistration");
+        }
+    }
+
+    private boolean containsBeanDefinition(ConfigurableListableBeanFactory beanFactory, String... beans) {
+        return Arrays.stream(beans).allMatch(b -> beanFactory.containsBeanDefinition(b));
+    }
+}
